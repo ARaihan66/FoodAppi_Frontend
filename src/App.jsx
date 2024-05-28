8cd2f68c5e0357b2d25ea1d90b0570ea5c2decbb
Abu Raihan <aburaihancse066@gmail.com>
2024-05-28 18:14:25 +0600

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Home/>
+    <div className='app'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        {/*<Route path='/menu' element={<Home/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home/>} />*/}
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../../assets/food_asset/food-appi-logo.png";
+import { Link } from "react-router-dom";
+import search_icon from "../../assets/food_asset/search_icon.png";
+import basket_icon from "../../assets/food_asset/basket_icon.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [menu, setMenu] = useState("home");
 
-export default Navbar
+  return (
+    <div className="flex justify-between items-center py-[20px]">
+      <Link to="/">
+        <img src={logo} alt="" className="w-[200px] h-[100px]" />
+      </Link>
+      <ul className="flex gap-6 text-xl font-semibold">
+        <li
+          onClick={() => setMenu("home")}
+          className={`${menu === "home" ? "underline" : ""}`}
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          onClick={() => setMenu("about")}
+          className={`${menu === "about" ? "underline" : ""}`}
+        >
+          <Link to="/">About</Link>
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={`${menu === "menu" ? "underline" : ""}`}
+        >
+          <Link to="/">Menu</Link>
+        </li>
+        <li
+          onClick={() => setMenu("contact")}
+          className={`${menu === "contact" ? "underline" : ""}`}
+        >
+          <Link to="/">Contact Us</Link>
+        </li>
+      </ul>
+
+      <div className="flex gap-8 relative ">
+        <img src={search_icon} alt="" />
+        <div>
+          <img src={basket_icon} alt="" />
+          <div className="absolute bg-orange-900 min-h-[10px] min-w-[10px] rounded-full top-[-16px] left-[82px] text-white">
+            300
+          </div>
+        </div>
+        <button className="bg-orange-500 border-solid	border-black rounded-md py-[5px] px-[10px] transition duration-300  hover:text-white">
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
