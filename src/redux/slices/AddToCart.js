7c31fdde726637d073b0dfed126ba66e0b0751d4
Abu Raihan <aburaihancse066@gmail.com>
2024-06-04 01:33:20 +0600

    Add to cart funtionality added

--- src/redux/slices/AddToCart.js
+++ src/redux/slices/AddToCart.js
@@ -13,11 +13,47 @@ export const CartSlice = createSlice({
 
             if(existedItem){
                 state.cart = state.cart.map((item)=>{
-                    return item.id === action.payload.id ? {item, qty: item.qty + 1} : item;
+                    return item.id === action.payload.id ? {item, qty: item.qty + 1} : null;
                 })
             }else{
                 state.cart.push(action.payload);
             }
-        }
+        },
+
+        removeFromCart: (state, action)=>{
+           state.cart = state.cart.filter((item)=> item.id !== action.payload);
+        },
+
+        incrementQty:(state, action)=>{
+          state.cart = state.cart.map((item)=>{
+            return item.id === action.payload.id ? {...item, qty:item.qty + 1 } : null
+          })
+        },
+        decrementQty:(state, action)=>{
+            state.cart = state.cart.map((item)=>{
+              return item.id === action.payload.id ? {...item, qty:item.qty - 1 } : null
+            })
+          },
+
+        totalPrice : (state, action)=>{
+             state.cart = state.cart.map((item)=>{
+                return item.id === action.payload.id ? {...item, price: item.price * item.qty}
+             })
+          }
     }
 })
+
+
+// Action creators are generated for each case reducer function
+export const {
+    addToCart,
+    removeFromCart,
+    incrementQty,
+    decrementQty,
+    totalPrice,
+  } = CartSlice.actions;
+  
+  export default CartSlice.reducer;
+  
+
+
